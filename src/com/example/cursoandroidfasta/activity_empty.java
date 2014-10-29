dd60370b3285f5a2a72c7dba8ff15adbe0b80f03
Gonzalo <benoffi7@gmail.com>
2014-10-29 16:37:13 -0300

    Clase 2

---
+++ src/com/example/cursoandroidfasta/activity_empty.java
@@ -0,0 +1,16 @@
+package com.example.cursoandroidfasta;
+
+import com.example.cursoandroidfasta.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class activity_empty extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		setContentView(R.layout.activity_empty);
+		super.onCreate(savedInstanceState);
+	}
+}
